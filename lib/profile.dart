5fa206ba12fee522e9ef6fd59549c3c5ad3581e8
ParkEunSeo <peswitch6@gmail.com>
2022-08-17 16:07:11 +0900

    dd

--- lib/profile.dart
+++ lib/profile.dart
@@ -1,21 +1,21 @@
 import 'package:flutter/material.dart';
 
 
-class profile extends StatefulWidget {
-  const profile({Key? key}) : super(key: key);
+class Profile extends StatefulWidget {
+  const Profile({Key? key}) : super(key: key);
 
   @override
-  State<profile> createState() => _profileState();
+  State<Profile> createState() => _ProfileState();
 }
 
-class _profileState extends State<profile> {
+class _ProfileState extends State<Profile> {
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: font,
+      theme: ThemeData(fontFamily: 'Kopub'),
       home: Scaffold(
-
+        appBar: AppBar(),
       ),
     );
   }
