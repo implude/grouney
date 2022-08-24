0d265370f2f7783c1ca2fbe94b64154e58a554ae
이동헌 <dongheon0916@dimigo.hs.kr>
2022-08-24 21:07:44 +0900

    feat: add home page

--- lib/home.dart
+++ lib/home.dart
@@ -1,11 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:grouney/main.dart';
+import 'package:intl/intl.dart';
 
 class Home extends StatelessWidget {
   final _dataList = [
-    _GroupData("그룹명", ["사람1", "사람2", "사람3"]),
-    _GroupData("임플루드", ["김석환", "김성훈", "박은서", "이동헌"])
+    _GroupData(
+        name: "그룹명",
+        people: ["사람1", "사람2", "사람3"],
+        dateRange: DateTimeRange(start: DateTime.now(), end: DateTime.now()),
+        destination: "부산"),
+    _GroupData(
+        name: "임플루드",
+        people: ["김석환", "김성훈", "박은서", "이동헌"],
+        dateRange: DateTimeRange(start: DateTime.now(), end: DateTime.now()),
+        destination: "부산"),
+    _GroupData(
+        name: "아예",
+        people: ["환석김", "재신오", "승연김"],
+        dateRange: DateTimeRange(start: DateTime.now(), end: DateTime.now()),
+        destination: "천춘")
   ];
 
   Home({Key? key}) : super(key: key);
@@ -30,7 +44,7 @@ class Home extends StatelessWidget {
                       MaterialStateProperty.all(Palatte.myskyskyblue),
                 ),
                 onPressed: () {},
-                child: CircleAvatar(
+                child: const CircleAvatar(
                     radius: 28,
                     backgroundColor: Colors.black,
                     child: CircleAvatar(
@@ -48,6 +62,7 @@ class Home extends StatelessWidget {
         padding: EdgeInsets.symmetric(
             vertical: context.heightTransformer(dividedBy: 20)),
         child: ListView.separated(
+            physics: const BouncingScrollPhysics(),
             itemBuilder: (_, index) =>
                 Center(child: _GroupItem(data: _dataList[index])),
             separatorBuilder: (_, __) =>
@@ -65,23 +80,76 @@ class _GroupItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(children: [
-      Image.asset("assets/images/group_outline.png"),
-      Padding(
-        padding: const EdgeInsets.only(left: 10),
-        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-          Text(data.name,
-              style: const TextStyle(fontSize: 24, fontFamily: "KBIZgo")),
-          Text(data.people.join(" "))
-        ]),
-      )
-    ]);
+    return Container(
+        width: context.widthTransformer(dividedBy: 1.2),
+        height: context.heightTransformer(dividedBy: 9.6),
+        decoration: const BoxDecoration(
+            image: DecorationImage(
+                scale: 0.1,
+                image: AssetImage("assets/images/group_outline.png"))),
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+              horizontal: context.widthTransformer(dividedBy: 10)),
+          child: Row(children: [
+            Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      Text(data.name,
+                          style: const TextStyle(
+                              fontSize: 20,
+                              fontFamily: "KBIZgo",
+                              fontWeight: FontWeight.bold)),
+                      const Icon(
+                        Icons.location_on,
+                        color: Palatte.myblue,
+                        size: 40,
+                      ),
+                      Column(children: () {
+                        final formatter = DateFormat("M/dd");
+                        final range = data.dateRange;
+                        return [
+                          "${formatter.format(range.start)}~${formatter.format(
+                              range.end)}",
+                          "${data.destination} 여행"
+                        ]
+                            .map((e) =>
+                            Text(e,
+                                style: const TextStyle(
+                                    color: Palatte.myblue,
+                                    fontFamily: "KBIZgo",
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 10)))
+                            .toList();
+                      }())
+                    ],
+                  ),
+                  Text(data.people.join(", "),
+                      style: const TextStyle(
+                          color: Color(0xFFC8C8C8),
+                          fontSize: 10,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: "KBIZgo")),
+                ]),
+            const Expanded(child: SizedBox.shrink()),
+            Image.asset("assets/images/부산.png", scale: 6)
+          ]),
+        ));
   }
 }
 
 class _GroupData {
   final String name;
   final List<String> people;
+  final DateTimeRange dateRange;
+  final String destination;
 
-  _GroupData(this.name, this.people);
+  const _GroupData({
+    required this.name,
+    required this.people,
+    required this.dateRange,
+    required this.destination,
+  });
 }
