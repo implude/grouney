ea1696acc2dd19d429bbfea39c21485118e8cf48
ParkEunSeo <peswitch6@gmail.com>
2022-10-05 16:11:13 +0900

    feeCal

--- lib/grouney/feeCal.dart
+++ lib/grouney/feeCal.dart
@@ -29,35 +29,44 @@ class _feeCalState extends State<feeCal> {
   Map iconData = <int, String>{
     1:'dd',
   };
-  // static const data = [
-  //   '아이스크림',
-  //   'OO고기 무한리필',
-  //   '부산롯월',
-  //   '불꽃놀이',
-  //   '수상레저',
-  // ];
 
-  Column feeList (int num,){
-    return Column(
-      children: [
-        Row(
-          children: [
-            Icon(Icons.dashboard),  //변경필요
-            SizedBox(width: MediaQuery.of(context).size.width*0.03,),
-            Text(indexData[num]),
-            SizedBox(width: MediaQuery.of(context).size.width*0.03,),
-            Text('-'),
-            SizedBox(width: MediaQuery.of(context).size.width*0.03,),
-            Text('${feeData[num]}')
-          ],
-        ),
-        SizedBox(height: MediaQuery.of(context).size.height*0.01,)
-      ],
-    );
-  }
+
+
 
   @override
   Widget build(BuildContext context) {
+    const fee = [
+      {
+        'icon':'-',
+        'title':'iceCream',
+        'fee':'500'
+      },
+      {
+        'icon':'-',
+        'title':'hihihi',
+        'fee':'500'
+      },
+      {
+        'icon':'-',
+        'title':'tezst',
+        'fee':'500'
+      },
+      {
+        'icon':'-',
+        'title':'eeeee',
+        'fee':'500'
+      },
+      {
+        'icon':'-',
+        'title':'ddddddddddd',
+        'fee':'500'
+      },
+      {
+        'icon':'-',
+        'title':'iceCasfdasdream',
+        'fee':'500'
+      },
+    ];
     return MaterialApp(
       theme: ThemeData(fontFamily: 'Kopub'),
       home: Scaffold(
@@ -66,35 +75,90 @@ class _feeCalState extends State<feeCal> {
           leading: Icon(Icons.arrow_back),
           backgroundColor: Palatte.myskyblue,),
         body: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            SingleChildScrollView(
-              child: Column(
-                children: [
-                  for(int num = 1; num<5;num++) feeList(num)
-                ],
+            Container(
+              child: ListView.builder(
+                scrollDirection: Axis.vertical,
+                shrinkWrap: true,
+                padding: const EdgeInsets.all(8),
+                itemCount: fee.length,
+                itemBuilder: (BuildContext context, int index){
+                  return Container(
+                    height: MediaQuery.of(context).size.height*0.1,
+                    padding: EdgeInsets.all(10),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(Icons.abc),
+                        SizedBox(width: MediaQuery.of(context).size.width*0.1,),
+                        Container(
+                          width: MediaQuery.of(context).size.width*0.3,
+                          child: RichText(
+                            text: TextSpan(
+                              style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold, color: Colors.black),
+                              text: '${fee[index]['title']}',
+                            ),
+                          ),
+                        ),
+                        SizedBox(width: MediaQuery.of(context).size.width*0.15,),
+                        Container(
+                          width: MediaQuery.of(context).size.width*0.2,
+                          child: RichText(
+                            textAlign: TextAlign.end,
+                            text: TextSpan(
+                              text: '₩${fee[index]['fee']}',
+
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  );
+                },
               ),
-            )
-            // ListView.builder(
-            //   itemCount: fee.length,
-            //     itemBuilder: (BuildContext context, int index){
-            //     return Container(
-            //       height: MediaQuery.of(context).size.height*0.03,
-            //       child: Row(
-            //         children: [
-            //           Icon(Icons.dashboard),
-            //           SizedBox(width: MediaQuery.of(context).size.width*0.03,),
-            //           Text(data[index]),
-            //           SizedBox(width: MediaQuery.of(context).size.width*0.03,),
-            //           Text('-'),
-            //           SizedBox(width: MediaQuery.of(context).size.width*0.03,),
-            //           Text('${fee[index]}')
-            //         ],
-            //       )
-            //     );
-            //     })
+            ),
+            TextButton(onPressed: (){},
+              style: ,
+                child: Text('비용 추가하기',
+                  style: TextStyle(
+                    fontFamily: 'KBIZgo', fontSize: 15, fontWeight: FontWeight.w700
+                  ),),
+            ),
           ],
         ),
       ),
     );
   }
 }
+
+// class FeeList extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context){
+//     return ListView.separated(
+//       itemCount: ,
+//         itemBuilder: (context, index){
+//
+//
+//         },
+//         separatorBuilder: separatorBuilder,
+//     );
+//   }
+// }
+// class Item{
+//   int fee;
+//   String index;
+//   String icon;
+//   Item(this.fee, this.index,this.icon);
+// }
+// class feeTile extends StatelessWidget{
+//   feeTile(this._item);
+//   final Item _item;
+//   @override
+//   Widget build(BuildContext context){
+//     return ListTile(
+//       leading: Icon(Icons.abc), //변경필요
+//       title: Text('${_item.index}-₩${_item.fee}'),
+//     );
+//   }
+// }
